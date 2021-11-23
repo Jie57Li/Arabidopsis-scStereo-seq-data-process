f9f81e218b6d29127ec070f137101fe44851085f
Jie57Li <94822224+Jie57Li@users.noreply.github.com>
2021-11-23 10:17:45 +0800

    Add files via upload

---
+++ r42.Mesophyll_Pattern.R
@@ -0,0 +1,103 @@
+###### Expression pattern analysis of all cells and Enrichment
+
+# 1. Library
+#-------------------------------------------------------------------------------
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("Mfuzz")                   
+# browseVignettes("Mfuzz")                          
+library(Mfuzz)
+library(Seurat)
+library(DOSE)
+library(org.At.tair.db)
+library(topGO)
+library(clusterProfiler)
+library(pathview)
+library(ggplot2)
+setwd("Projects/Fig4")
+
+# 2. Load data
+#-------------------------------------------------------------------------------
+obj <- readRDS("../RDS/Run-PCA-871cells.rds")
+s_merge <- subset(obj,cell_type %in% c("sponge","palisad"))
+
+all <- FindAllMarkers(s_merge, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+all <- unique(all$gene)
+AverageExp <- as.data.frame(AverageExpression(s_merge)$RNA)
+AverageExp <- AverageExp[all,]
+
+# 3. mfuzz for pattern
+#-------------------------------------------------------------------------------
+data <- AverageExp
+gene_tpm <- data.matrix(data)
+eset <- new("ExpressionSet",exprs = gene_tpm)
+gene.r <- filter.NA(eset, thres = 0.25)
+## mean fill NA values
+gene.f <- fill.NA(gene.r, mode="mean")
+## knn/wknn方法表现更好，但是计算起来比较复杂
+# gene.f <- fill.NA(gene.r, mode="knn")
+gene.f <- fill.NA(gene.r, mode="wknn")
+## 过滤标准差为0的基因
+tmp <- filter.std(gene.f, min.std = 0)
+## Standardise
+gene.s <- standardise(tmp)
+## Cluster numbers
+c <- 3
+## Estimate best m
+m <- mestimate(gene.s)
+## Cluster
+cl <- mfuzz(gene.s, c = c, m = m)
+## Check genes numbers in each cluster
+cl$size
+## Export gene cluster
+write.table(cl$cluster, "Mesophyll/Mesophyll_clusters.txt", 
+            quote = F, row.names = T, col.names = F, sep = "\t")
+
+## Draw line chart
+pdf("Mesophyll/Mesophyll_patterns.pdf", width = 6, height = 7)
+#p <- 
+mfuzz.plot(gene.s, cl, mfrow=c(1,3), new.window = F, 
+           time.labels = c("0", "1", "2", "3"), min.mem = 0.41)
+dev.off()
+
+# 4. Enrichment
+#-------------------------------------------------------------------------------
+genes <- as.data.frame(cl$cluster)
+colnames(genes) <- "cluster"
+genes$id <- rownames(genes)
+file <- c("Cluster1","Cluster2","Cluster3")
+#cluster <- c("1","2","3","4")
+for (i in 1:length(file)) {
+  dir.create(paste("Mesophyll/Enrichment/",i,sep = ""))
+  id2 <- as.vector(genes[which(genes$cluster == i),2])
+  # Transform gene names
+  genelits = bitr(id2, 
+                  fromType="TAIR",
+                  toType="ENTREZID", 
+                  OrgDb="org.At.tair.db")
+  id <- as.vector(genelits[,2])
+  
+  ## 3. GO
+  ego.all <- enrichGO(
+    gene          = id,
+    keyType = "ENTREZID",
+    OrgDb         = org.At.tair.db,
+    ont           = "BP",    # options : "CC" "BP" "MF" "ALL"
+    pAdjustMethod = "BH",
+    pvalueCutoff  = 1,
+    qvalueCutoff  = 1,
+    readable      = FALSE) # whether mapping gene ID to gene Name
+  
+  barplot(ego.all, showCategory=20,title="EnrichmentGO_ALL")
+  # dotplot(ego.all,showCategory=20,title="EnrichmentGO_ALL_dot")
+  write.table(ego.all,paste("Mesophyll/Enrichment/",i,"/Cluster",i,"_GO.results.txt",sep = ""),row.names = FALSE,sep = "\t")
+  
+  ## 4. KEGG
+  ekk.all <- enrichKEGG(gene         = id2,
+                        organism     = "ath",
+                        pvalueCutoff = 1)
+  barplot(ekk.all, showCategory=20,title="EnrichmentGO_ALL")
+  write.table(ekk.all, paste("Mesophyll/Enrichment/",i,"/Cluster",i,"_KEGG.results.txt",sep = ""),row.names = FALSE,sep="\t")
+  #   
+}
+
---
+++ r51.monocle2.R
@@ -0,0 +1,141 @@
+###### Trajectory analysis of vascular cells
+
+# 1. Install and Library Packages 
+#-------------------------------------------------------------------------------
+# install.packages("BiocManager")
+# BiocManager::install("monocle")
+library(monocle)
+library(RColorBrewer)
+library(ggplot2)
+library(Seurat)
+library(ggpubr)
+setwd("J:/BGI/Projects/2021/2.植物空间组/2.Analyses/02.AT-leaves/")
+
+# 2. Find marker gene 
+#-------------------------------------------------------------------------------
+rds1 <- readRDS("a0.RDS/reassigned_merged_umap_res0.4_0313v2(1)(1).rds")
+## Vascular marker
+sub <- rds1[,grep(pattern = "vascu", rownames(rds1@meta.data))]
+vas <- FindMarkers(sub, ident.1 = 0, ident.2 = 3, min.pct = 0.14)
+vas <- rownames(vas[which(vas$p_val<0.05),])
+data <- data.frame(sub@assays$RNA@counts)
+names(data) <- gsub("\\.", "-", names(data))
+type <- data.frame(sub@meta.data)
+type <- type[c("cell_type", "reassign_7cut")]
+type$reassign_7cut <-paste("Position", type$reassign_7cut, sep="")
+
+# 3. Data Sets
+#-------------------------------------------------------------------------------
+expr_matrix <- data[,rownames(type)]   # expression matrix
+gene_annotation <- data.frame(gene_id=rownames(expr_matrix),
+                              gene_short_name=rownames(expr_matrix))
+rownames(gene_annotation) <- gene_annotation[,1]   # gene annotation
+sample_sheet <- type   # corresponding cell annotation information
+
+# 4. Preprocessing
+#-------------------------------------------------------------------------------
+pos <- which(rownames(gene_annotation) %in% rownames(expr_matrix))
+gene_annotation <- gene_annotation[pos,]
+expr_matrix <- expr_matrix[rownames(gene_annotation),rownames(sample_sheet)]
+pd <- new("AnnotatedDataFrame", data = sample_sheet)   # cell annotation object
+fd <- new("AnnotatedDataFrame", data = gene_annotation)   # gene annotation object
+
+## Choose appropriate distribution according to expression data type
+## UMI (default input): for negative binomal distribution data
+cd <- newCellDataSet(as(as.matrix(expr_matrix), "sparseMatrix"), phenoData = pd,
+                     featureData = fd, expressionFamily = negbinomial.size(),
+                     lowerDetectionLimit = 0.5)
+cd <- estimateSizeFactors(cd)   # help eliminate differences in mRNA capture between cells
+cd <- estimateDispersions(cd)   # for subsequent differential expression analysis
+cd <- detectGenes(cd, min_expr = 0.5)   # counts the number of genes expressed in each cell
+expressed_genes <- row.names(subset(fData(cd), num_cells_expressed > nrow(sample_sheet) * 0.01))
+
+# 5. Construction of Single Cell Pseudotime 
+#-------------------------------------------------------------------------------
+ordering_genes <- vas
+ordering_genes <- intersect(ordering_genes, expressed_genes)
+cd <- setOrderingFilter(cd, ordering_genes)
+## plot_ordering_genes(cd)
+
+## Order Cells by Progress
+cd <- reduceDimension(cd, max_components = 2, method = 'DDRTree')
+cd <- orderCells(cd, reverse = F)   # reverse = F by default
+
+## generate color palette
+getPalette <- colorRampPalette(brewer.pal(6, "Set1"))
+
+# 6. Visualize in reduced dimensional Space 
+#-------------------------------------------------------------------------------
+# Visualize Cell Trajectory
+color1 <- c("#CC503B","#0072B5","#D68518","#55A368","#7976B2","#779AAE","#F9DB8D","#F54E91")
+
+# 7. Visualize in reduced dimensional Space 
+#-------------------------------------------------------------------------------
+# Visualize Cell Trajectory
+pc1 <- plot(plot_cell_trajectory(cd, show_cell_names = F, color_by = "cell_type", cell_size = 2.5)) +
+  scale_color_manual(values = "#B9C8A0")
+pc2 <- plot(plot_cell_trajectory(cd, show_cell_names = F, color_by = "reassign_7cut", cell_size = 2.5))
+# scale_color_manual(values = color1))
+pc3 <- plot(plot_cell_trajectory(cd, show_cell_names = F,
+                                 color_by = "Pseudotime", cell_size = 2.5) +
+              scale_color_viridis_c())
+# pc4 <- plot(plot_cell_trajectory(cd, show_cell_names = F, color_by = "State",cell_size = 3))
+
+p <- ggarrange(pc1, pc2, pc3, nrow = 1)
+
+## Output Cell Trajectory
+pdf("4.results/5.Monocle/3.Vascular/Plots/updated/Vascular_full_trajectory_T_0423.pdf", width = 15, height = 4)
+p
+dev.off()
+
+# 8. Do heatmap 
+#-------------------------------------------------------------------------------
+# Whole
+diff_test_res <- differentialGeneTest(cd[ordering_genes,],
+                                      fullModelFormulaStr = "~sm.ns(Pseudotime)")
+sig_gene_names <- row.names(subset(diff_test_res, qval < 0.4, pval < 0.05))
+
+tst <- plot_pseudotime_heatmap(cd[sig_gene_names,],
+                               num_clusters = 3,
+                               cores = 1,
+                               show_rownames = F)
+pdf("4.results/5.Monocle/3.Vascular/Plots/updated/VasDEG_by_pseudotime_nonames_0320.pdf",width = 5, height = 5)
+plot_pseudotime_heatmap(cd[sig_gene_names,],
+                        num_clusters = 3,
+                        cores = 1,
+                        show_rownames = F)
+dev.off()
+
+# 9. Plot canonical genes
+#-------------------------------------------------------------------------------
+library(ggpubr)
+## Main figures
+## Start
+cds_subset <- cd[c("AT1G78370"),]
+pg1 <- plot_genes_in_pseudotime(cds_subset, color_by = "reassign_7cut")
+## Middle
+cds_subset <- cd[c("AT5G44580"),]
+pg2 <- plot_genes_in_pseudotime(cds_subset, color_by = "reassign_7cut")
+## End
+cds_subset <- cd[c("AT1G09932"),]
+pg3 <- plot_genes_in_pseudotime(cds_subset, color_by = "reassign_7cut")
+p1 <- ggarrange(pg1, pg2, pg3, nrow = 1)
+## Supplementary figures
+cds_subset <- cd[c("AT1G26630"),]
+pg1 <- plot_genes_in_pseudotime(cds_subset, color_by = "reassign_7cut")
+## Middle
+cds_subset <- cd[c("AT5G42530"),]
+pg2 <- plot_genes_in_pseudotime(cds_subset, color_by = "reassign_7cut")
+## End
+cds_subset <- cd[c("AT5G40690"),]
+pg3 <- plot_genes_in_pseudotime(cds_subset, color_by = "reassign_7cut")
+cds_subset <- cd[c("AT4G33980"),]
+pg4 <- plot_genes_in_pseudotime(cds_subset, color_by = "reassign_7cut")
+p2 <- ggarrange(pg1, pg2, pg3, pg4, nrow = 1)
+
+pdf("4.results/5.Monocle/3.Vascular/Canonical_genes_20210317_main_v2.pdf", width = 12, height = 3)
+p1
+dev.off()
+pdf("4.results/5.Monocle/3.Vascular/Canonical_genes_20210317_supplementary_v2.pdf", width = 16, height = 3)
+p2
+dev.off()
